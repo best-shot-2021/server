cf69ce857a284068bf5b895d1b387ae9c9634226
Hans-0101 <hyhsumin@naver.com>
2022-01-15 09:03:01 +0000

    feat:adding voice analyzer

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ gpus = tensorflow.config.experimental.list_physical_devices('GPU')
 tensorflow.config.experimental.set_virtual_device_configuration(gpus[0], [tensorflow.config.experimental.VirtualDeviceConfiguration(memory_limit=6144)])
 
 
-face_classifier = FaceShapeEvaluator('/root/servers/server_1/model_ver2.h5')
+face_classifier = FaceShapeEvaluator('/root/server/model_ver2.h5')
 
 
-@app.route('/getmethod', methods=['GET'])
+@app.route('/getmethod', methods=['POST'])
 def sample():
     return 'hi'
 
 
-@app.route('/face_classifier', methods=['POST'])
-def uploader_file():
+@app.route('/face_classifier', methods=['POST', 'GET'])
+def uploader_img_file():
     if request.method == 'POST':
         try:
             f = request.files['img_file']
@@ -32,8 +32,26 @@ def uploader_file():
         result = face_classifier.evaluate(path)
         os.remove(path)
 
+        # return jsonify({"face":result})
         return result
 
+
+@app.route('/voice_analyzer', methods=['POST', 'GET'])
+def uploader_voice_file():
+    if request.method == 'POST':
+        try:
+            f = request.files['voice_file']
+        except:
+            return 'File is missing', 404
+
+        f.save(secure_filename(f.filename))
+        path = os.path.realpath(f.filename)
+
+        mysp=__import__("my-voice-analysis")
+        result = mysp.myspsyl(f.filename, path)
+        os.remove(path)
+
+    return str(result)
 
 
 @app.route('/userLogin', methods = ['POST'])
--- test.py
+++
@@ -1,5 +0,0 @@
-from evaluator import FaceShapeEvaluator
-
-tester = FaceShapeEvaluator('/root/servers/server_1/model_ver2.h5')
-result = tester.evaluate('/root/servers/server_1/round_test.jpg')
-print(result)
